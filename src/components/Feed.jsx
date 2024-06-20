f189b9ceaa562d4a7eb9f9aaa32021357041bd61
Ajay Susanth <ajaysusanth10@gmail.com>
2024-06-20 17:42:52 +0530

    navbar and sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,38 @@
+import { useState,useEffect } from "react"
+import { Stack,Box,Typography } from "@mui/material"
+import {Sidebar,Videos} from './'
+
+
+
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack
+    sx={{flexDirection: {sx: "column", md: "row"}}}
+    >
+      <Box
+        sx={{height:{sx:'auto', md:"92vh"}, borderRight:"1px solid #3d3d3d", px: {sx:0,md:2}}}
+      >
+        <Sidebar/>
+        <Typography
+          className = 'copyright' variant="body2"
+          sx ={{mt:1.5,color:"#fff"}}
+        >
+          Copyright 2024 JSM Media
+        </Typography>
+
+      </Box>
+
+      <Box p={2} sx={{overflowY:'auto', height:'90vh',flex:2}}>
+        <Typography variant="h4" fontWeight="bold" sx={{color:'white'}} mb={2} >
+          New <span style={{color:'#FC1503', }}>Videos</span>
+
+        </Typography>
+
+        <Videos/>
+
+      </Box>
+      
+    </Stack>
   )
 }
-export default Feed
+export default Feed 
